79339249094ce9c2c5a578c8fda0b413343c2df0
Roy Reznik <royreznik@gmail.com>
2025-04-04 22:32:11 +0300

    feat(infra): uv

---
+++ scripts/install_moon.sh
@@ -0,0 +1,4 @@
+bash <(curl -fsSL https://moonrepo.dev/install/moon.sh)
+
+echo "You should add moon to the path in order for everything to work"
+echo 'echo "export PATH=$HOME/.moon/bin:$PATH" > ~/.bashrc'
